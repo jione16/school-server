fd29541fc46b84c6bbdfa123880e0be58adf6c00
jione16 <onebornawkward@gmail.com>
2020-09-22 16:09:11 +0700

    done

--- resources/views/report/statistic.blade.php
+++ resources/views/report/statistic.blade.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Statistic</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" media="all" />
     <style>
 table {
   font-family: arial, sans-serif;
   border-collapse: collapse;
   width: 100%;
+}
+
+.text-center {
+    text-align: center;
 }
 
 td, th {
