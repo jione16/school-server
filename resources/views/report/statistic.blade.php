af81dc4269dd1c148675dcd60cdb4d17ad3fb7d8
jione16 <onebornawkward@gmail.com>
2020-09-22 16:01:58 +0700

    test

--- resources/views/report/statistic.blade.php
+++ resources/views/report/statistic.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Statistic</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" media="all" />
+    <link rel="stylesheet" href="/css/style.css" media="all" />
     <style>
 table {
   font-family: arial, sans-serif;
